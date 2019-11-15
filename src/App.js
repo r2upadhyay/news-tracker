fe64a34ad0677b8400d419c6721bf66071d9763c
tmouhamed <s.toqaa@gmail.com>
2019-11-15 12:22:41 -0500

    get stories from hacker news api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import './Main.scss';
+import Axios from 'axios';
+import Moment from 'react-moment';
 
-class Main extends React.Component{
+class Main extends React.Component {
+    state = {
+        story: [],
+    }
+
+    baseUrl = 'https://hacker-news.firebaseio.com/v0';
+    newStoriesUrl = `${this.baseUrl}/newstories.json`;
+    storyUrl = `${this.baseUrl}/item`;
+
+    getId = () => {
+        Axios.get(`${this.newStoriesUrl}`)
+            .then(response => {
+                const articleIds = response.data.slice(0, 10);
+
+                const axiosRequests = articleIds.map(id => {
+                    return this.getStory(id);
+                });
+
+                Axios.all(axiosRequests)
+                    .then(responses => {
+                        this.setState({
+                            story: responses
+                        })
+                    })
+            })
+    }
+    getStory = (storyId) => {
+        return Axios.get(`${this.storyUrl}/${storyId}.json`)
+    }
+
+    componentDidMount() {
+        this.getId();
+    }
+
     render() {
-        return(
+        console.log(this.state);
+
+        return (
             <section className="main">
-                <article className="main__card">
-                    <span className="main__card-category">Music</span>
-                    <h2 className="main__card-title">The first line of Lorem Ipsum</h2>
-                    <p className="main__card-description">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
-                </article>
-                <article className="main__card">
-                    <span className="main__card-category">Music</span>
-                    <h2 className="main__card-title">The first line of Lorem Ipsum</h2>
-                    <p className="main__card-description">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
-                </article>
+                {this.state.story.map((story) => {
+                    return(
+                        <article className="main__card">
+                            <span className="main__card-category">Music</span>
+                            <h2 className="main__card-title">{story.data.title}</h2>
+                            <p className="main__card-description">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
+                            <span className="main__card-author">By {story.data.by}</span>
+                            <span className="main__card-time"><Moment format="YYYY/MM/DD">{story.data.time}</Moment></span>
+                        </article>
+                    )
+                })
+            }
             </section>
         );
     }
