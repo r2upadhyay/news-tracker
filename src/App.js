71ea723103cae36c75329b542fe3a2484e5aa85f
tmouhamed <s.toqaa@gmail.com>
2019-11-15 15:10:23 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main/Main';
+import Header from './components/Header/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Main />
       </div>
     );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Main.scss';
 import Axios from 'axios';
-import Moment from 'react-moment';
+import Timestamp from 'react-timestamp';
 
 class Main extends React.Component {
     state = {
         story: [],
+        storyKids: []
     }
 
     baseUrl = 'https://hacker-news.firebaseio.com/v0';
@@ -18,15 +19,15 @@ class Main extends React.Component {
         var month = dateObj.getUTCMonth() + 1; //months from 1-12
         var day = dateObj.getUTCDate();
         var year = dateObj.getUTCFullYear();
-        var newdate;
-        return newdate = year + "/" + month + "/" + day;
+        var hours = dateObj.getHours()
+
+        return year + "/" + month + "/" + day + hours;
     }
 
     getId = () => {
         Axios.get(`${this.newStoriesUrl}`)
             .then(response => {
-                const arrayLength = response.data.length;
-                const articleIds = response.data.slice(0, 10);
+                const articleIds = response.data.slice(0, 100);
                 const axiosRequests = articleIds.map(id => {
                     return this.getStory(id);
                 });
@@ -34,7 +35,7 @@ class Main extends React.Component {
                 Axios.all(axiosRequests)
                     .then(responses => {
                         this.setState({
-                            story: responses
+                            story: responses,
                         })
                     })
             })
@@ -48,17 +49,18 @@ class Main extends React.Component {
     }
 
     render() {
+        console.log(this.state)
         return (
             <section className="main">
                 {this.state.story.map((story) => {
                     return (
-                        <article className="main__card">
+                        <article className="main__card" key={story.data.id}>
                             <span className="main__card-category">{story.data.type}</span>
                             <h2 className="main__card-title">{story.data.title}</h2>
-                            <p className="main__card-description">{story.data.text}The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
+                            <p className="main__card-a"><a className="main__card-link" href={story.data.url}>{story.data.url}</a></p>
                             <div className="main__card-details">
-                                <span className="main__card-details-author">By: {story.data.by}</span>
-                                <span className="main__card-details-time">{this.getTheDate(story.data.time)}</span>
+                                <span className="main__card-details-author">{story.data.score} points by: {story.data.by} . {story.data.descendants} comments</span>
+                                <span className="main__card-details-time"><Timestamp relative date={story.data.time} /></span>
                             </div>
                         </article>
                     )
