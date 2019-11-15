57b9143cdab5f1494226bb5329c2401d77e603d6
tmouhamed <s.toqaa@gmail.com>
2019-11-14 23:53:57 -0500

    test data added!

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Main.scss';
 
 class Main extends React.Component{
     render() {
-        return(null);
+        return(
+            <section className="main">
+                <article className="main__card">
+                    <span className="main__card-category">Music</span>
+                    <h2 className="main__card-title">The first line of Lorem Ipsum</h2>
+                    <p className="main__card-description">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
+                </article>
+                <article className="main__card">
+                    <span className="main__card-category">Music</span>
+                    <h2 className="main__card-title">The first line of Lorem Ipsum</h2>
+                    <p className="main__card-description">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero </p>
+                </article>
+            </section>
+        );
     }
 }
 export default Main;
