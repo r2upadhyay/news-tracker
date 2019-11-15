0a5224c376dc25e2b86af8374474233649bba21b
Ravi Upadhyay <r2upadhyay@outlook.com>
2019-11-15 14:42:44 -0500

    Revised header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './styles.scss';
-import SearchBox from '../Search-box/index';
-import Button from '../Button/index';
+import image from '../../image/news-1644696_1280.png'
 
 const Header = (props) => {
     return (
         <header className="header">
             <div className="container header-content">
-                <div className="header-name">
-                    <h1>News Tracker</h1>
+                <div className="header-img">
+                    <img src={image} className="header-content__logo" alt="logo"/>
                 </div>
-                <div className="header__details">
-                    <div className="header__details--search">
-                        <SearchBox placeholder="Enter date"/>
-                    </div>
-                    <div className="header__details--button">
-                        <Button icon="btn">Search</Button>
-                    </div>
-                </div>
+                <nav className="header__details">
+                    <a href="" className="jobs">Jobs</a>
+                    <a href ="" className="headlines">Headlines</a>
+                </nav>
             </div>
         </header>
     )
